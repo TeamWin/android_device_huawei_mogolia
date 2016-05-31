f6fef15213e43b9c471526f2d1a5addee2d81032
Paul O'Brien <paul@modaco.com>
2016-05-31 16:44:32 +0100

    Re-added blank on boot

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,4 +65,5 @@ TW_BRIGHTNESS_PATH := "/sys/devices/platform/hisi_fb.1048577/leds/lcd_backlight0
 TW_CUSTOM_BATTERY_PATH := "/sys/devices/fff34000.pmic/battery.90/power_supply/Battery"
 TW_MAX_BRIGHTNESS := 255
 BOARD_HAS_NO_SELECT_BUTTON := true
+TW_SCREEN_BLANK_ON_BOOT := true
 TW_EXCLUDE_SUPERSU := true
